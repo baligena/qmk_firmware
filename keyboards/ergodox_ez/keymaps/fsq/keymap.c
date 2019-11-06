c3497c668b8a7fc7898854c97aa40be671238bb8
felipe <you@example.com>
2019-11-06 13:06:13 -0500

    layer 0 switch up and down arrows

--- keyboards/ergodox_ez/keymaps/fsq/keymap.c
+++ keyboards/ergodox_ez/keymaps/fsq/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DELETE , KC_Q         , KC_W     , KC_E            , KC_R    , KC_T     , TG(1)					 , TG(1)    , KC_Y        , KC_U        , KC_I     , KC_O       , KC_P             , KC_BSLASH ,
     KC_BSPACE , KC_A         , KC_S     , KC_D            , KC_F    , KC_G     , KC_H					 , KC_J     , KC_K        , KC_L        , LT(2     , KC_SCOLON) , LGUI_T(KC_QUOTE) ,
     KC_LSHIFT , LCTL_T(KC_Z) , KC_X     , KC_C            , KC_V    , KC_B     , KC_HYPR				 , KC_MEH   , KC_N        , KC_M        , KC_COMMA , KC_DOT     , RCTL_T(KC_SLASH) , KC_RSHIFT ,
-    LT(1      , KC_GRAVE)    , KC_QUOTE , LALT(KC_LSHIFT) , KC_LEFT , KC_RIGHT , KC_UP					 , KC_DOWN  , KC_LBRACKET , KC_RBRACKET , MO(1)    ,
+	LT(1      , KC_GRAVE)    , KC_QUOTE , LALT(KC_LSHIFT) , KC_LEFT , KC_RIGHT , KC_DOWN 				 ,KC_UP  , KC_LBRACKET , KC_RBRACKET , MO(1)    ,
 															 LALT_T(KC_APPLICATION),KC_LGUI,			KC_LALT,LCTL_T(KC_ESCAPE),
 																					KC_HOME,			KC_PGUP,
 																  KC_SPACE,KC_BSPACE,KC_END,			KC_PGDOWN,KC_TAB,KC_ENTER
